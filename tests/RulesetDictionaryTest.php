e78c19447cc32159796a806611ce632b3ebc0511
Geoff Appleby <geoff@gapple.ca>
2020-02-01 11:22:01 -0800

    Stop skipping tests with duplicate keys
    draft-ietf-httpbis-header-structure-15 changed the behaviour for
    duplicate keys to overwrite previous values instead of failing

--- tests/RulesetDictionaryTest.php
+++ tests/RulesetDictionaryTest.php
@@ -5,12 +5,4 @@ namespace gapple\Tests\StructuredHeaders;
 class RulesetDictionaryTest extends RulesetTest
 {
     protected $ruleset = 'dictionary';
-
-    protected $skipParsingRules = [
-        'duplicate key dictionary' => 'Duplicate dictionary keys should overwrite previous value.',
-    ];
-
-    protected $skipSerializingRules = [
-        'duplicate key dictionary' => 'Duplicate dictionary keys should overwrite previous value.',
-    ];
 }
--- tests/RulesetKeyGeneratedTest.php
+++ tests/RulesetKeyGeneratedTest.php
@@ -5,14 +5,4 @@ namespace gapple\Tests\StructuredHeaders;
 class RulesetKeyGeneratedTest extends RulesetTest
 {
     protected $ruleset = 'key-generated';
-
-    protected $skipParsingRules = [
-        '0x2c in dictionary key' => 'comma is valid character in dictionary',
-        '0x3b in parameterised list key' => 'semicolon is valid character in parameter list',
-    ];
-
-    protected $skipSerializingRules = [
-        '0x2c in dictionary key' => 'comma is valid character in dictionary',
-        '0x3b in parameterised list key' => 'semicolon is valid character in parameter list',
-    ];
 }
