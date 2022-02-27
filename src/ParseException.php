a4d7e5ed10c684eb6ffdddc001bc519511c9f992
Ignace Nyamagana Butera <nyamsprod@gmail.com>
2022-02-27 11:40:25 +0100

    Adding Exception Marker

--- src/ParseException.php
+++ src/ParseException.php
@@ -1,7 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace gapple\StructuredFields;
 
-class ParseException extends \RuntimeException
+use RuntimeException;
+
+class ParseException extends RuntimeException implements StructuredFieldError
 {
 }
--- src/SerializeException.php
+++ src/SerializeException.php
@@ -1,7 +1,11 @@
 <?php
 
+declare(strict_types=1);
+
 namespace gapple\StructuredFields;
 
-class SerializeException extends \RuntimeException
+use RuntimeException;
+
+class SerializeException extends RuntimeException implements StructuredFieldError
 {
 }
