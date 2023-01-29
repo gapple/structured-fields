56b85d97156260bd82db6a3ec32b7c3f81e89a9c
Geoff Appleby <geoff@gapple.ca>
2023-01-29 00:45:46 -0800

    Use Date ruleset from httpwg

--- tests/DateTest.php
+++ tests/DateTest.php
@@ -2,55 +2,14 @@
 
 namespace gapple\Tests\StructuredFields;
 
-use gapple\StructuredFields\Date;
-
 /**
- * Test Date parsing and serializing.
- *
- * @todo Replace with httpwg test cases when available
- *       https://github.com/httpwg/structured-field-tests/pull/83
+ * Additional Date parsing and serializing tests.
  */
 class DateTest extends RulesetTest
 {
     protected function rulesetDataProvider(): array
     {
         $rules = [
-            [
-                "name" => "date - 1970-01-01 00:00:00",
-                "raw" => ["@0"],
-                "header_type" => "item",
-                "expected" => [new Date(0), new \stdClass()],
-            ],
-            [
-                "name" => "date - 2022-08-04 01:57:13",
-                "raw" => ["@1659578233"],
-                "header_type" => "item",
-                "expected" => [new Date(1659578233), new \stdClass()],
-            ],
-            [
-                "name" => "date - 1917-05-30 22:02:47",
-                "raw" => ["@-1659578233"],
-                "header_type" => "item",
-                "expected" => [new Date(-1659578233), new \stdClass()],
-            ],
-            [
-                "name" => "date - 2^31",
-                "raw" => ["@2147483648"],
-                "header_type" => "item",
-                "expected" => [new Date(2147483648), new \stdClass()],
-            ],
-            [
-                "name" => "date - 2^32",
-                "raw" => ["@4294967296"],
-                "header_type" => "item",
-                "expected" => [new Date(4294967296), new \stdClass()],
-            ],
-            [
-                "name" => "date - decimal",
-                "raw" => ["@1659578233.12"],
-                "header_type" => "item",
-                "must_fail" => true,
-            ],
             [
                 "name" => "date - large int",
                 "raw" => ["@1234567890123456"],
---
+++ tests/RulesetDateTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace gapple\Tests\StructuredFields;
+
+class RulesetDateTest extends RulesetTest
+{
+    protected $ruleset = 'date';
+}
