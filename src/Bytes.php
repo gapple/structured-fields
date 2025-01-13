4fa1e4db66838bbfead0b69546233937ac3470de
Geoff Appleby <geoff@gapple.ca>
2025-01-13 00:21:35 -0800

    Explicit implements Stringable

--- src/Bytes.php
+++ src/Bytes.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace gapple\StructuredFields;
 
-class Bytes
+class Bytes implements \Stringable
 {
     public function __construct(private readonly string $value)
     {
--- src/Token.php
+++ src/Token.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace gapple\StructuredFields;
 
-class Token
+class Token implements \Stringable
 {
     public function __construct(private readonly string $value)
     {
